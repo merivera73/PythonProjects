1d559d2d0104b4d1d7ec2971807da5bb12283bfa
Manny <merivera73@gmail.com>
2026-02-22 00:53:46 -0800

    Playing with PDF files

--- pdf-playground/pdf.py
+++ pdf-playground/pdf.py
@@ -1,13 +1,23 @@
 import pypdf
  
-template = pypdf.PdfReader(open('superduper.pdf', 'rb'))
-watermark = pypdf.PdfReader(open('water.pdf', 'rb'))
-output = pypdf.PdfWriter()
+# template = pypdf.PdfReader(open('superduper.pdf', 'rb'))
+# watermark = pypdf.PdfReader(open('dummy.pdf', 'rb'))
+# output = pypdf.PdfWriter()
  
-for i in range(len(template.pages)):
-    page = template.pages[i]
-    page.merge_page(watermark.pages[0])
-    output.add_page(page)
+# for i in range(len(template.pages)):
+#     page = template.pages[i]
+#     page.merge_page(watermark.pages[0])
+#     output.add_page(page)
  
-with open('watermaked_output.pdf', 'wb') as outputFile:
-    output.write(outputFile)
+with open('dummy.pdf', 'rb') as outputFile:
+    reader = pypdf.PdfReader(outputFile)
+    page = reader.get_page(0)
+    print(page.rotate(90))
+    writer = pypdf.PdfWriter()
+    with open('tilt.pdf', 'wb') as new_outputFile:
+        writer.write(new_outputFile)
+        
+        
+        
+    
+# output.write
