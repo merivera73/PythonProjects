4c2026a42436d65234d895299688c68a1ed81404
Manny <merivera73@gmail.com>
2026-03-10 00:00:53 -0700

    adding more features

--- password-playground/checkmypass.py
+++ password-playground/checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(query_char):
@@ -8,11 +9,29 @@ def request_api_data(query_char):
     if res.status_code != 200:
         raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
     return res
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:        
+        if h == hash_to_check:
+            return count
+    return 0
+
+
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5_char)
-    print(first5_char, tail)
-    return response
+    return get_password_leaks_count(response, tail)
 
-pwned_api_check('123')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times.... you should change your password!!!')
+        else:
+            print(f'{password} was not found so you are good to go with it')
+    return 'All Done!!!!'    
+        
+
+main(sys.argv[1:])
