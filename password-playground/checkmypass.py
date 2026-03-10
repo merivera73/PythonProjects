734b02a42b1378a1e69144269f9d5409dccdc7c2
Manny <merivera73@gmail.com>
2026-03-09 23:26:35 -0700

    corrected issue with password checker

--- password-playground/checkmypass.py
+++ password-playground/checkmypass.py
@@ -1,12 +1,18 @@
 import requests
+import hashlib
 
-url = 'https://api.pwnedpasswords.com/range/' + 'e6b6a'
-res = requests.get(url)
-print(res)
 
 def request_api_data(query_char):
-    pass
-
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+    return res
 def pwned_api_check(password):
-    #check password if it exists in API response
-    pass
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    print(first5_char, tail)
+    return response
+
+pwned_api_check('123')
